9112d1e7187035ffee8a991a07153ad25bcca3d8
Shivendra1233 <132485033+Shivendra1233@users.noreply.github.com>
2023-11-18 20:24:08 -0800

    F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,7 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name, 4, 1, 9, isManual, "Formula 1", 1);
+        super(name, 4, 1, 9, isManual, "Formula1", 1);
     }
 
     public void accelerate(int rate) {
